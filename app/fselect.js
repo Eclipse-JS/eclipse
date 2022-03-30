c625b9b9c9ae2c189f7e025bfe8d5487c55b0ce7
greysoh <greysoh@pm.me>
2022-03-30 14:54:05 -0400

    feature: Migrates everything to switch statements.

--- app/fselect.js
+++ app/fselect.js
@@ -1,11 +1,10 @@
-if (args[0] == undefined) {
-  kernel.stdout("Error: No command specified.\n");
-  kernel.stdout("Usage: fselect <command> [args]\n");
-  kernel.stdout("Type 'fselect help' for more information.");
-  return;
+let arg = args[0];
+
+if (args[1] !== undefined) {
+  arg += ` ${args[1]}`;
 }
 
-switch (args[0]) {
+switch (arg) {
   case "help":
     kernel.stdout("fselect version 0.1.0 - Not a furry package manager.\n");
     kernel.stdout("Usage: fselect <command> [args]\n");
@@ -28,41 +27,13 @@ switch (args[0]) {
   case "search":
     kernel.stdout("Not implemented!");
     break;
-  default:
-    if (args[0] == "repo" && args[1] == "add") {
-      if (args[2] == undefined) {
-        kernel.stdout("Error: No repository specified.\n");
-        kernel.stdout("Usage: execJS add <repo>\n");
-        kernel.stdout("Type 'fselect help' for more information.");
-      } else {
-        let url = args[2];
-
-        if (!url.startsWith("http://") || !url.startsWith("https://")) {
-          if (!url.startsWith("/")) {
-            url = "/" + url + "/manifest.json";
-          } else {
-            url = "https://" + url;
-          }
-        }
-
-        let items = localStorage.getItem("fselect_manifest").toString().split("");
-        items.pop();
-
-        if (items.length - 1 == 0) {
-          items.push('"' + url + '"');
-        } else {
-          items.push(',"' + url + '"');
-        }
-
-        items.push("]");
-
-        localStorage.setItem("fselect_manifest", items.join(""));
-
-        kernel.stdout("Added repository '" + url + "' to init.");
-      }
-    } else if (args[0] == "repo" && args[1] == "remove") {
+  case "repo add":
+    if (args[2] == undefined) {
+      kernel.stdout("Error: No repository specified.\n");
+      kernel.stdout("Usage: fselect repo add <repo>\n");
+      kernel.stdout("Type 'fselect help' for more information.");
+    } else {
       let url = args[2];
-      let items = [];
 
       if (!url.startsWith("http://") || !url.startsWith("https://")) {
         if (!url.startsWith("/")) {
@@ -72,16 +43,57 @@ switch (args[0]) {
         }
       }
 
-      for (item of JSON.parse(localStorage.getItem("fselect_manifest"))) {
-        if (item != url) {
-          items.push(item);
-        } else {
-          kernel.stdout("Removed repository '" + url + "' from init.");
-        }
+      let items = localStorage.getItem("fselect_manifest").toString().split("");
+      items.pop();
+
+      if (items.length - 1 == 0) {
+        items.push('"' + url + '"');
+      } else {
+        items.push(',"' + url + '"');
       }
 
-      localStorage.setItem("fselect_manifest", JSON.stringify(items));
-    } else if (args[0] == "repo" && args[1] == "update") {
-      kernel.stdout("Not implemented.");
+      items.push("]");
+
+      localStorage.setItem("fselect_manifest", items.join(""));
+
+      kernel.stdout("Added repository '" + url + "' to init.");
     }
+    break;
+  case "repo remove":
+    if (args[2] == undefined) {
+      kernel.stdout("Error: No repository specified.\n");
+      kernel.stdout("Usage: fselect repo remove <repo>\n");
+      kernel.stdout("Type 'fselect help' for more information.");
+      break;
+    }
+    
+    let url = args[2];
+    let items = [];
+
+    if (!url.startsWith("http://") || !url.startsWith("https://")) {
+      if (!url.startsWith("/")) {
+        url = "/" + url + "/manifest.json";
+      } else {
+        url = "https://" + url;
+      }
+    }
+
+    for (item of JSON.parse(localStorage.getItem("fselect_manifest"))) {
+      if (item != url) {
+        items.push(item);
+      } else {
+        kernel.stdout("Removed repository '" + url + "' from init.");
+      }
+    }
+
+    localStorage.setItem("fselect_manifest", JSON.stringify(items));
+    break;
+  case "repo update":
+    kernel.stdout("Not implemented!");
+    break;
+  default:
+    kernel.stdout("Error: No command specified.\n");
+    kernel.stdout("Usage: fselect <command> [args]\n");
+    kernel.stdout("Type 'fselect help' for more information.");
+    break;
 }
