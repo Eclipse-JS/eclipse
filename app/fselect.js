f4ae079560f4ae497907a072189c4d329fb970cb
greysoh <greysoh@pm.me>
2022-03-30 18:23:37 +0000

    fix: Attempts to implement switch statements.

--- app/fselect.js
+++ app/fselect.js
@@ -5,21 +5,30 @@ if (args[0] == undefined) {
   return;
 }
 
-if (args[0] == "help") {
-  kernel.stdout("fselect version 0.1.0 - Not a furry package manager.\n");
-  kernel.stdout("Usage: fselect <command> [args]\n");
-  kernel.stdout("Available commands:\n");
-  kernel.stdout("  help - Displays this help message.\n");
-  kernel.stdout("  repo add <repo> - Adds a repository.\n");
-  kernel.stdout("  repo remove <repo> - Removes a repository.\n");
-  kernel.stdout("  repo update - Updates repository listings.\n");
-  kernel.stdout("  install - Installs app.\n");
-  kernel.stdout("  remove - Removes app.\n");
-  kernel.stdout("  search - Searches for an app.\n");
-  kernel.stdout("  manifest-details - List valid approved repositories.");
-  return;
-}
-
+switch(args[0]) {
+  case "help":
+    kernel.stdout("fselect version 0.1.0 - Not a furry package manager.\n");
+    kernel.stdout("Usage: fselect <command> [args]\n");
+    kernel.stdout("Available commands:\n");
+    kernel.stdout("  help - Displays this help message.\n");
+    kernel.stdout("  repo add <repo> - Adds a repository.\n");
+    kernel.stdout("  repo remove <repo> - Removes a repository.\n");
+    kernel.stdout("  repo update - Updates repository listings.\n");
+    kernel.stdout("  install - Installs app.\n");
+    kernel.stdout("  remove - Removes app.\n");
+    kernel.stdout("  search - Searches for an app.\n");
+    kernel.stdout("  repo md - List valid approved repositories.");
+    break;
+  case "install":
+    kernel.stdout("Not implemented!");
+    break;
+  case "remove":
+    kernel.stdout("Not implemented!");
+    break;
+  case "search":
+    kernel.stdout("Not implemented!");
+    break;
+  default:
 if (args[0] == "repo" && args[1] == "add") {
   if (args[2] == undefined) {
     kernel.stdout("Error: No repository specified.\n");
@@ -74,18 +83,5 @@ if (args[0] == "repo" && args[1] == "add") {
   localStorage.setItem("fselect_manifest", JSON.stringify(items));
 } else if (args[0] == "repo" && args[1] == "update") {
   kernel.stdout("Not implemented.");
-} else if (args[0] == "install") {
-  kernel.stdout("Not implemented.");
-} else if (args[0] == "uninstall") {
-  kernel.stdout("Not implemented.");
-} else if (args[0] == "search") {
-  kernel.stdout("Not implemented.");
-} else if (args[0] == "manifest-details") {
-  kernel.stdout("Fetching from server...\n\n");
-
-  let data = await axios.get("manifest_details.txt");
-
-  kernel.stdout(data.data);
-} else {
-  kernel.stdout("Not a valid argument.");
+}    
 }
