5f6667b71e3ac5c6e0f6c1dafbb720dcce6b4fc7
greysoh <greysoh@pm.me>
2022-03-31 17:27:33 +0000

    feature: Migrates stdin and out to the init.

--- demo.js
+++ demo.js
@@ -5,12 +5,125 @@ let Applications = [];
 // Reserved.
 let windowServer = {};
 
+// Keyboard stuff
+let isCtrl = false;
+let isC = false;
+
+let isCtrlC = false;
+
+document.addEventListener("keydown", function (e) {
+  if (isKbdEnabled) {
+    let bannedKbdKeys = [
+      "Control",
+      "Alt",
+      "WakeUp",
+      "Meta",
+      "OS",
+      "Page",
+      "Arrow",
+      "Shift",
+      "Escape",
+      "CapsLock",
+      "Tab",
+      "Home",
+      "End",
+      "Insert",
+      "Delete",
+      "Unidentified",
+      "F1",
+      "F2",
+      "F0",
+      "ContextMenu",
+      "AudioVolumeDown",
+      "AudioVolumeUp",
+    ];
+
+    if (isCtrl && e.keyCode == "67") {
+      return;
+    }
+
+    if (isCtrl && e.keyCode == "86") {
+      return;
+    }
+
+    if (e.key == "Enter") {
+      isKbdEnabled = false;
+      return;
+    }
+
+    if (e.key == "Backspace") {
+      vstd = vstd.slice(0, -1);
+      document.getElementsByClassName("main")[0].innerHTML = sanitize(stdout + vstd).replaceAll("\n", "<br>").replaceAll(" ", "&nbsp;");
+      document.getElementsByClassName("main")[0].scrollTo(0, document.getElementsByClassName("main")[0].scrollHeight);
+      return;
+    }
+
+    for (kbd of bannedKbdKeys) {
+      if (e.key.startsWith(kbd)) {
+        return;
+      }
+    }
+
+    vstd += e.key;
+
+    document.getElementsByClassName("main")[0].innerHTML = sanitize(stdout + vstd).replaceAll("\n", "<br>").replaceAll(" ", "&nbsp;");
+    document.getElementsByClassName("main")[0].scrollTo(0, document.getElementsByClassName("main")[0].scrollHeight);
+  }
+});
+
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+async function ctrlC() {
+  while (true) {
+    await sleep(100);
+    if (isCtrl && isC) window.location.reload();
+  }
+}
+
+ctrlC(); // broken for now
+
 addEventListener("DOMContentLoaded", async function () {
+  console.log("earlycon at console.log();");
+  console.log("loading textfb-friendly kernel...");
+
+  let KernelLocal = kernel;
+
+  KernelLocal.ver += ".kdpatch" // keyboard driver patch
+
+  KernelLocal.stdout = function (...args) {
+    let argv = args.join(" ");
+
+    if (argv == "jsKernelReq$cls") {
+      document.getElementsByClassName("main")[0].innerText = "";
+      stdout = "";
+      return;
+    }
+    stdout += argv;
+    document.getElementsByClassName("main")[0].innerHTML = sanitize(stdout).replaceAll("\n", "<br>").replaceAll(" ", "&nbsp;");
+
+    document.getElementsByClassName("main")[0].scrollTo(0, document.getElementsByClassName("main")[0].scrollHeight);
+  }
+
+  KernelLocal.stdin = async function () {
+    isKbdEnabled = true;
+
+    while (isKbdEnabled !== false) {
+      await sleep(100);
+    }
+
+    let vsh = vstd;
+    vstd = "";
+
+    KernelLocal.stdout(vsh, "\n");
+
+    return vsh;
+  }
+  
   kernel.initExec(async function () {
+    let kernel = KernelLocal;
+    console.log("loaded. booting with patched kernel...");
     kernel.stdout("femInit v0.1.1\n");
     kernel.stdout("Checking if system config is sane...\n");
     
@@ -49,8 +162,7 @@ addEventListener("DOMContentLoaded", async function () {
       Applications.push({ name: app.name, version: app.version, function: new AsyncFunction("args", atob(app.function)) });
     }
 
-    kernel.stdout("\n");
-    kernel.stdout("Applications loaded.\n\n");
+    kernel.stdout("\nApplications loaded.\n\n");
 
     let shellIndex = 0;
 
