73bf280ce67db2e7bbc7f06f98f363dceb03341b
greysoh <greysoh@pm.me>
2022-05-01 20:16:10 -0400

    feature: Adds nuke toggle in rc.

--- app/rc.js
+++ app/rc.js
@@ -8,7 +8,8 @@ if (args[0] == undefined) {
 if (args[0] == "help") {
     kernel.stdout("rc - Modify system settings.\n");
     kernel.stdout("Commands:\n");
-    kernel.stdout("startupApp - Changes the Application to launch at startup.");
+    kernel.stdout("startupApp - Changes the Application to launch at startup.\n");
+    kernel.stdout("nuke - Force resets all packages before boot on startup.");
     return;
 } else if (args[0] == "startupApp") {
     if (args[1] == undefined) {
@@ -22,6 +23,33 @@ if (args[0] == "help") {
 
     kernel.stdout("Successfully set autostart program.");
     return;
+} else if (args[0] == "nuke") {
+    if (args[1] !== "toggle" && args[1] !== "on" && args[1] !== "off") {
+        kernel.stdout("No value specified! [toggle, on, off]\n");
+        kernel.stdout("Usage: rc [command] <args>\n");
+        kernel.stdout("To get help, run 'rc help'.");
+        return;
+    }
+
+    if (args[1] == "toggle") {
+        if (localStorage.getItem("nuke.rc") == "true") {
+            localStorage.removeItem("nuke.rc");
+            kernel.stdout("Nuke is now off.");
+        } else {
+            localStorage.setItem("nuke.rc", "true");
+            kernel.stdout("Nuke is now on.");
+        }
+    }
+
+    if (args[1] == "on") {
+        localStorage.setItem("nuke.rc", "true");
+        kernel.stdout("Nuke is now on.");
+    }
+
+    if (args[1] == "off") {
+        localStorage.removeItem("nuke.rc");
+        kernel.stdout("Nuke is now off.");
+    }
 } else {
     kernel.stdout("Invalid command.\n");
     kernel.stdout("Usage: rc [command] <args>\n");
