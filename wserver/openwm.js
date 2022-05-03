009dc0fb088d4e5506a350c8c94e470c5fba9fd9
greysoh <greysoh@pm.me>
2022-05-03 17:19:55 -0400

    feature: Adds terminal on startup if not detected.

--- wserver/openwm.js
+++ wserver/openwm.js
@@ -2,52 +2,59 @@ kernel.stdout("OpenWM is starting up...\n");
 kernel.stdout(" - Checking if WindowServer is running...\n");
 
 if (typeof windowServer.newWindow == "function") {
-    kernel.stdout(" - WindowServer is already running!");
-    return;
+  kernel.stdout(" - WindowServer is already running!");
+  return;
 }
 
 for (i of Applications) {
-    if (i.name == "wserv") {
-        kernel.pexec("wserv", i.function, []);
-    }
+  if (i.name == "wserv") {
+    kernel.pexec("wserv", i.function, []);
+  }
 }
 
 if (typeof windowServer.newWindow !== "function") {
-    kernel.stdout(" - WindowServer failed to start!");
-    return;
+  kernel.stdout(" - WindowServer failed to start!");
+  return;
 }
 
 windowServer.setDesktopWallpaper("#000000");
 
 let hasStarted = false;
 
-let terminal = Applications.find(app => { return app.name == "terminal" })
+let terminal = Applications.find((app) => {
+  return app.name == "terminal";
+});
 
 kernel.stdout(" - Starting panel...\n");
 
-let nepo = Applications.find(app => { return app.name == "nepo" });
+let nepo = Applications.find((app) => {
+  return app.name == "nepo";
+});
+
 kernel.pexec(nepo.name, nepo.function);
 
 kernel.stdout(" - Running init tasks...\n");
 
-if (localStorage.getItem("openwm_init.cfg") !== null) {
-    let init = localStorage.getItem("openwm_init.cfg");
-    init = init.split(";");
+if (localStorage.getItem("openwm_init.cfg") !== null)
+  localStorage.setItem("openwm_init.cfg", "terminal");
 
-    for (i of init) {
-        kernel.stdout(` - Running init task: ${i}...`);
-        try {
-            let app = Applications.find(app => { return app.name == i });
+let init = localStorage.getItem("openwm_init.cfg");
+init = init.split(";");
 
-            if (app !== undefined) {
-                kernel.pexec(app.name, app.function);
-            }
-        } catch (e) {
-            kernel.stdout(": FAIL");
-            console.error(e);
-        }
+for (i of init) {
+  kernel.stdout(` - Running init task: ${i}...`);
+  try {
+    let app = Applications.find((app) => {
+      return app.name == i;
+    });
 
-        kernel.stdout("\n");
+    if (app !== undefined) {
+      kernel.pexec(app.name, app.function);
     }
-}
+  } catch (e) {
+    kernel.stdout(": FAIL");
+    console.error(e);
+  }
 
+  kernel.stdout("\n");
+}
