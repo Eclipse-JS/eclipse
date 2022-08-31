8f01449397343e3c440216ac685ca064a7158ff0
greysoh <greysoh@pm.me>
2022-08-31 18:29:28 -0400

    Security: Fixes kernel crashes and escelation bugs.

--- init/security.js
+++ init/security.js
@@ -18,6 +18,12 @@ function genKernel(localAccount) {
       },
       get(name) {
         if (name == "genkernel") return;
+        
+        if (name == "users" && account.permLevel != 0) {
+          const user = Kernel.extensions.get(name);
+
+          return { parseUser: user.parseUser };
+        }
 
         return Kernel.extensions.get(name, function(){ return account; });
       }
@@ -36,11 +42,12 @@ function genKernel(localAccount) {
     accounts: {
       async elevate(username, password) {
         let newProfile = await users.parseUser(username);
-        newProfile.username = username;
   
         if (!newProfile) {
-          throw "Invalid user";
+          return false;
         }
+
+        newProfile.username = username;
   
         if (account.permLevel == 0) {
           account = newProfile;
