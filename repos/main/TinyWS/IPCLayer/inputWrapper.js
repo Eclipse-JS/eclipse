d0e1c5481940c0eeff07ee787d92e9d0d2b7908a
greysoh <greysoh@pm.me>
2022-09-11 08:06:09 -0400

    TinyWS: Makes input wrapper use switch statements.

--- repos/main/TinyWS/IPCLayer/inputWrapper.js
+++ repos/main/TinyWS/IPCLayer/inputWrapper.js
@@ -1,29 +1,35 @@
-if (input.event == "FetchRequest") {
-  if (input.subevent == "genCanvas") return generateCanvas;
-} else if (input.event == "FetchWindowData") {
-  const find = windows.find(i => i.uuid == input.uuid);
+switch (input.event) {
+  case "FetchRequest": {
+    if (input.subevent == "genCanvas") return generateCanvas;
+  }
+  
+  case "FetchWindowData": {
+    const find = windows.find(i => i.uuid == input.uuid);
 
-  if (!find) {
-    return { error: "ENOTFOUND" };
-  } else {
-    const canvas = find.fetchCanvas();
+    if (!find) {
+      return { error: "ENOTFOUND" };
+    } else {
+      const canvas = find.fetchCanvas();
 
-    return {
-      success: true,
-      xy: [canvas.style.top, canvas.style.left],
-      wh: [canvas.width, canvas.height]
+      return {
+        success: true,
+        xy: [canvas.style.top, canvas.style.left],
+        wh: [canvas.width, canvas.height]
+      }
     }
   }
-} else if (input.event == "MoveWindow") {
-  const find = windows.find(i => i.uuid == input.uuid);
 
-  if (!find) {
-    return { error: "ENOTFOUND" };
-  } else {
-    const canvas = find.fetchCanvas();
-    canvas.style.top = input.top;
-    canvas.style.left = input.left;
+  case "MoveWindow": {
+    const find = windows.find(i => i.uuid == input.uuid);
+
+    if (!find) {
+      return { error: "ENOTFOUND" };
+    } else {
+      const canvas = find.fetchCanvas();
+      canvas.style.top = input.top;
+      canvas.style.left = input.left;
     
-    return { success: "NoSkill" };
+      return { success: "NoSkill" };
+    }
   }
 }
