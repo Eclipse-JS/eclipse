4a2a9174b272bb62b92a5504af2962a22290c50d
greysoh <greysoh@pm.me>
2022-10-22 16:32:03 -0400

    Dusk: Switch to partially properly* measuring text.
    *: Still using magic numbers, lol.
    
    also happy 200 commits lmao

--- repos/main/DuskTerm/draw/functions.js
+++ repos/main/DuskTerm/draw/functions.js
@@ -1,3 +1,27 @@
+/**
+ * Measures width and height of a string with new lines 
+ * @param {string || object} i Text to measure
+ * @returns {object} width: Number, height: Number
+ */
+function measureFullText(i) {
+  const text = typeof i == "string" ? i.split("\n") : i;
+  const size = {
+    width: 0,
+    height: 0
+  }
+
+  const magicNum = 8; // Offset for the height. Guessed using the monospace font of Firefox on Windows 10.
+
+  for (i of text) {
+    const measureResult = ctx.measureText(i);
+    if (measureResult.width > size.width) size.width = measureResult.width;
+
+    size.height += measureResult.actualBoundingBoxAscent + measureResult.actualBoundingBoxDescent + magicNum; // https://stackoverflow.com/questions/1134586/how-can-you-find-the-height-of-text-on-an-html-canvas
+  }
+
+  return size;
+}
+
 function fillText(text, count) {
   ctx.font = fontSize + "px monospace";
   ctx.fillStyle = theme.styles.general.accents.white["background-color"];
@@ -19,7 +43,7 @@ function redraw(textData) {
   let text = [];
 
   for (i of rawText) {
-    if (i.length > maxChars) {
+    if (ctx.measureText(i).width > canvasElement.width) {
       const overLength = Math.floor(i.length / maxChars);
 
       for (var j = 1; j <= overLength; j++) {
@@ -33,7 +57,7 @@ function redraw(textData) {
     }
   }
 
-  while (text.length > maxLines) text.shift();
+  while (measureFullText(text).height > canvasElement.height) text.shift();
 
   for (var i = 1; i < maxLines+1; i++) {
     if (!text[i-1]) break;
