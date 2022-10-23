590244f5847448edfa6d3f3624dd34c345454cf9
greysoh <greysoh@pm.me>
2022-10-22 20:11:09 -0400

    feature: Adds eslint.
    Oh god kill me

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "ecmaFeatures": {
+          "globalReturn": true
+        }
+    },
+    "globals": {
+      "Kernel": true,
+      "argv": true
+    },
+    
+    "extends": "eslint:recommended",
+
+    "overrides": [],
+    "rules": {}
+}
