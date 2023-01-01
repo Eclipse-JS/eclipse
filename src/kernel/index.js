dfe942db20f746ae34e1313b7a8956b5c781eefc
greysoh <dev@hofers.cloud>
2022-12-31 23:40:02 -0500

    Kernel+TTY: Adds new TTY, for the new system, and sentry mode.
    Sentry mode is a lightweight antivirus to detect injected scripts in the
    main framebuffer (v2).
    
    The new TTY now utilizes the main framebuffer. More ported garbage
    coming soon! :D

--- src/kernel/index.js
+++ src/kernel/index.js
@@ -21,6 +21,15 @@ function createElementSec(elem) {
   return createdElement;
 }
 
+function sentry() {
+  const fbV2 = document.getElementById("framebuffer_v2");
+  if (fbV2.innerHTML.includes("<script>") || fbV2.innerHTML.includes("</script>")) {
+    panic("Security bypass remanents have been detected. You have been compromised.", "Kernel::Sentry");
+  }
+
+  setTimeout(sentry, 200);
+}
+
 document.getElementById("framebuffer_v2").createElement = createElementSec;
 document.createElement = createElementSec;
 
@@ -74,6 +83,9 @@ function panic(error, atLocation, trace) {
 function assert(test, msg) {
   if (!test) panic("Assertion failed!! " + msg, "KernelSpace::Anonymous");
 }
+
+console.log("Loading Sentry...");
+sentry();
 
 self.Kernel = {
   kernelLevel: {
