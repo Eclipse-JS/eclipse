de921459487f081da2954f7602bda6eae45b104c
greysoh <dev@hofers.cloud>
2023-03-07 20:59:41 -0500

    Dawn: Adds Windows 11 inspired theme.

--- repos/main/DawnUI/ThemeLoader.js
+++ repos/main/DawnUI/ThemeLoader.js
@@ -2,7 +2,8 @@ qb.enableRegularRequire();
 
 function getAllThemes() {
   const themes = [require("./Themes/groovy.dawn.json"),
-                  require("./Themes/arcticice.dawn.json")];
+                  require("./Themes/arcticice.dawn.json"),
+                  require("./Themes/eleven.dawn.json")];
 
   return themes;
 }
