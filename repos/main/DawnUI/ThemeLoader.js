86b63206a63f96da17a3d2d81cb8376b065801a9
greysoh <dev@hofers.cloud>
2023-03-07 21:00:37 -0500

    Dawn: Fixes formatting of getAllThemes.

--- repos/main/DawnUI/ThemeLoader.js
+++ repos/main/DawnUI/ThemeLoader.js
@@ -1,13 +1,15 @@
 qb.enableRegularRequire();
 
 function getAllThemes() {
-  const themes = [require("./Themes/groovy.dawn.json"),
-                  require("./Themes/arcticice.dawn.json"),
-                  require("./Themes/eleven.dawn.json")];
+  const themes = [
+    require("./Themes/groovy.dawn.json"),
+    require("./Themes/arcticice.dawn.json"),
+    require("./Themes/eleven.dawn.json")
+  ];
 
   return themes;
 }
 
 function getDefaultTheme() {
-  return VFS.existsSync("/etc/sonnesvr/dawn/theme", "folder") ? VFS.read("/etc/dawn.d/theme") : "Arctic Ice";
+  return VFS.existsSync("/etc/sonnesvr/dawn/theme", "folder") ? VFS.read("/etc/dawn.d/theme") : "Eleven (Light)";
 }
