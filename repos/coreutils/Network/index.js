eaa25131dd6a8b11e6a2529703fab0118a49354e
greysoh <dev@hofers.cloud>
2023-03-18 22:29:51 -0400

    Network: Adds code from jsnet-proto.

--- repos/coreutils/Network/index.js
+++ repos/coreutils/Network/index.js
@@ -1,18 +1,42 @@
+const { netAPI } = require("./WebCore/DeviceCore.mjs");
+
 qb.enableRegularRequire();
 
 const VFS = Kernel.extensions.get("Vfs");
 let hostname = VFS.existsSync("/etc/hostname", "file") ? VFS.read("/etc/hostname") : "localhost";
 
+require("./Functions/ExtensionExists.js");
+require("./Functions/ArrayAllocate.js");
+
+// Kaboom
+require("./WebCore/DeviceCore.mjs");
+
 if (!extensionExists("libnet")) {
   Kernel.extensions.load("libnet", {
-    setHostname: function(name) {
-      VFS.write("/etc/hostname", name);
-      hostname = name;
-    },
-    getHostname: function() {
-      return hostname;
+    connect: netAPI.connect,
+    listen: netAPI.listen,
+
+    core: {
+      hostname: {
+        set: function(name) {
+          VFS.write("/etc/hostname", name);
+          hostname = name;
+        },
+        get: function() {
+          return hostname;
+        }
+      },
+
+      devices: {
+        add: netAPI.core.addNetworkDevice,
+        get: netAPI.core.getNetworkDevices
+      },
+
+      helper: {
+        sanitize: netAPI.helperInternal.sanitizeIPMsg
+      }
     }
-  })
+  });
 } else {
   const input = Kernel.extensions.get("input");
 
