900a0aca42f41da02f1c104a9349ef1a207686cb
greysoh <dev@hofers.cloud>
2023-03-25 18:16:38 -0400

    feature: Adds warning message for missing IndexedDB support.

--- src/userspace/filesystem/backends/CloudaFS/!load.js
+++ src/userspace/filesystem/backends/CloudaFS/!load.js
@@ -4,6 +4,22 @@ require("./fs/index.mjs");
 const cloudaEnableDebugLog = false;
 
 async function loadClouda() {
+  if (!('indexedDB' in window)) {
+    const err = `
+!!! Error !!!
+
+You are attempting to load EclipseOS with CloudaFS (the Clouda filesystem).
+Your browser does not currently support IndexedDB, a required element to load CloudaFS.
+Therefore, I am going to set the filesystem backend to be 'gbrfs'.
+`;
+    localStorage["active_fs"] = "gbrfs";
+    alert(err);
+
+    window.location.reload();
+
+    return;
+  }
+
   const fsCreated = new CloudaFS("EclipsePROD_", cloudaEnableDebugLog);
   await fsCreated.init();
 
