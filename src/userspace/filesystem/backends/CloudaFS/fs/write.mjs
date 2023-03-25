29d7f17148e84002a1d264672a1a32e562f6fa1f
greysoh <dev@hofers.cloud>
2023-03-25 18:10:49 -0400

    fix: CORRECTLY fixes overwriting not working as intended.
    I can't believe it's not stupid:tm:!

--- src/userspace/filesystem/backends/CloudaFS/fs/write.mjs
+++ src/userspace/filesystem/backends/CloudaFS/fs/write.mjs
@@ -2,31 +2,27 @@ async function write(path, contents, fsIndexes, fsFiles) {
   const pathSplit = splitFilePath(path);
 
   const pathIndexSearch = await getAllPromise(fsIndexes);
-  const validateParentExists = pathIndexSearch.target.result.find((i) => i.path == pathSplit.directory && i.type == "folder");
-  const validateNotAlreadyExists = pathIndexSearch.target.result.find((i) => i.path == path);
-
   const fsIndexSearch = await getAllPromise(fsFiles);
-
-  if (validateNotAlreadyExists) {
-    const fsKey = fsIndexSearch.target.result.find((i) => i.path == path);
-
-    fsIndexes.delete(validateNotAlreadyExists.index);
-    fsFiles.delete(fsKey.index);
-  }
-
-  if (!validateParentExists) {
-    throw new Error("Parent directory does not exist!");
-  }
   
+  const validateParentExists = pathIndexSearch.target.result.find((i) => i.path == pathSplit.directory && i.type == "folder");
+  const validateNotAlreadyExists = pathIndexSearch.target.result.find((i) => i.path == path && i.type == "file");
+
+  const fsKey = fsIndexSearch.target.result.find((i) => i.path == path);
+
+  if (!validateParentExists) throw new Error("Parent directory does not exist!");
+
+  const indexIncrement = validateNotAlreadyExists ? validateNotAlreadyExists.index : pathIndexSearch.target.result.length + 1;
+  const filesIncrement = validateNotAlreadyExists ? fsKey.index : fsIndexSearch.target.result.length + 1;
+
   fsIndexes.put({
-    index: pathIndexSearch.target.result.length + 1,
+    index: indexIncrement,
     type: "file",
     path: path,
     owner: "tbd"
   });
 
   fsFiles.put({
-    index: fsIndexSearch.target.result.length + 1,
+    index: filesIncrement,
     path: path,
     contents: contents
   });
