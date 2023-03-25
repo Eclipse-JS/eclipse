6b84deb4cf3915ac3302e4aafc6abd010f17c090
greysoh <dev@hofers.cloud>
2023-03-25 16:13:26 -0400

    fix(wip): Adds removing existing elements in writing files.

--- src/userspace/filesystem/backends/CloudaFS/fs/write.mjs
+++ src/userspace/filesystem/backends/CloudaFS/fs/write.mjs
@@ -3,11 +3,18 @@ async function write(path, contents, fsIndexes, fsFiles) {
 
   const pathIndexSearch = await getAllPromise(fsIndexes);
   const validateParentExists = pathIndexSearch.target.result.find((i) => i.path == pathSplit.directory && i.type == "folder");
+  const validateNotAlreadyExists = pathIndexSearch.target.result.find((i) => i.path == path);
 
   const fsIndexSearch = await getAllPromise(fsFiles);
 
+  if (validateNotAlreadyExists) {
+    const fsKey = fsIndexSearch.target.result.find((i) => i.path == path);
+
+    fsIndexes.delete(validateNotAlreadyExists.index);
+    fsFiles.delete(fsKey.index);
+  }
+
   if (!validateParentExists) {
-    console.log(validateParentExists);
     throw new Error("Parent directory does not exist!");
   }
   
