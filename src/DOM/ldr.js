ec202df83c5f57a742664b4dc984b035da4b07ad
greysoh <dev@hofers.cloud>
2023-04-02 18:23:03 -0400

    feature: Adds warning for Safari users.
    The UI looks odd on there.

--- src/DOM/ldr.js
+++ src/DOM/ldr.js
@@ -1,3 +1,5 @@
+const isSafariBrowser = () => navigator.userAgent.indexOf('Safari') > -1 && navigator.userAgent.indexOf('Chrome') <= -1
+
 async function read(path) {
   const data = await fetch(path);
   const dataText = await data.text();
@@ -13,6 +15,13 @@ if (!localStorage.getItem("__init")) {
 if (!localStorage.getItem("krnl")) {
   const kernel = await read("kernel.js");
   localStorage.setItem("krnl", kernel);
+}
+
+if (isSafariBrowser()) {
+  alert(`ldr.js WARNING:
+I have detected that you are currently using the Safari browser.
+EclipseOS runs on Safari, but the UI is broken, possibly due to fonts.
+If you want the best experience, use the Eclipse reference browser, Firefox.`);
 }
 
 eval(localStorage.getItem("krnl"));
